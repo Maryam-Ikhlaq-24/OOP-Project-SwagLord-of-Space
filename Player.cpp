14171b25cd9ef72416b95a023ffcf9f033c3093b
Maryam-Ikhlaq-24 <Marium_Ikhlaq@outlook.com>
2024-07-27 23:35:39 +0500

    fixing texture errors

--- Player.cpp
+++ Player.cpp
@@ -2,12 +2,30 @@
 
 void Player::initTexture()
 {
-			// Load a texture from file
-	if (!this->texture.loadFromFile("Texture/ship.png"))
+	//Load a texture from file
+	if (!this->texture.loadFromFile("Texture/Ship.png"))
 	{
 		std::cout << "ERROR::PLAYER::INITTEXTURE::Could mot load texture file." << "\n";
+		
 	}
+
+	/*void Player::initTexture()
+	{
+		if (!this->texture.loadFromFile("Texture/ship.png"))
+		{
+			throw std::runtime_error("ERROR::PLAYER::INITTEXTURE::Could not load texture file.");
+		}
+	}
+void Player::initTexture()
+{
+	if (!this->texture.loadFromFile("Texture/Ship.png"))
+	{
+		throw std::runtime_error("ERROR::PLAYER::INITTEXTURE::Could not load texture file 'ship.png'. Check the file path.");
+	}
+}*/
+
 }
+
 
 void Player::initSprite()
 {
